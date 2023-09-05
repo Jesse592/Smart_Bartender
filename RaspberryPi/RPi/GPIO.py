ff28b0ad4d2cd51d8c1eb09178cb68e70ea493c2
Jesse <jm.krijgsman@student.avans.nl>
2023-09-05 11:01:27 +0200

    RP: Create windows RPi.GPIO mock

---
+++ RaspberryPi/RPi/GPIO.py
@@ -0,0 +1,19 @@
+HIGH = 1
+LOW = 0
+FALLING = 1
+BOARD = 1
+OUT = 1
+IN = 1
+
+def setmode(a):
+   return
+def setup(a, b, initial):
+   return
+def add_event_detect(a, b, callback, bouncetime):
+   return
+def output(a, b):
+   return
+def cleanup():
+   return
+def setwarnings(flag):
+   return
